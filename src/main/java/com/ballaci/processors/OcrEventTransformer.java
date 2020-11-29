54c00c5b1167c97ae41a4f3bd4180666d666f018
Armand Ballaci <aballaci@gmail.com>
2020-11-29 12:17:43 +0100

    Update OcrEventTransformer.java

--- src/main/java/com/ballaci/processors/OcrEventTransformer.java
+++ src/main/java/com/ballaci/processors/OcrEventTransformer.java
@@ -1,12 +1,13 @@
 package com.ballaci.processors;
 
 
-import com.ballaci.consumer.CustomAggregator;
+import com.ballaci.CustomSerdes;
+import com.ballaci.model.OcrAggregatedEvent;
 import com.ballaci.model.OcrReadyEvent;
 import com.ballaci.model.StoreItem;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.Transformer;
-import org.apache.kafka.streams.processor.Processor;
 import org.apache.kafka.streams.processor.ProcessorContext;
 import org.apache.kafka.streams.processor.PunctuationType;
 import org.apache.kafka.streams.state.KeyValueIterator;
@@ -15,10 +16,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
+@Slf4j
+public class OcrEventTransformer implements Transformer<String, OcrReadyEvent, KeyValue<String, OcrAggregatedEvent>> {
 
-public class OcrEventTransformer implements Transformer<String, OcrReadyEvent, OcrReadyEvent> {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(OcrEventTransformer.class);
     private ProcessorContext context;
     private KeyValueStore<String, StoreItem> kvStore;
 
@@ -39,47 +39,52 @@ public class OcrEventTransformer implements Transformer<String, OcrReadyEvent, O
         kvStore = (KeyValueStore) context.getStateStore(stateStoreName);
 
         // schedule a punctuate() method every second based on event-time
-        this.context.schedule(Duration.ofSeconds(1), PunctuationType.WALL_CLOCK_TIME, (timestamp) -> {
+        this.context.schedule(Duration.ofMillis(1000), PunctuationType.WALL_CLOCK_TIME, (timestamp) -> {
             try {
+                log.trace("Punctuating....");
                 KeyValueIterator<String, StoreItem> it = this.kvStore.all();
                 while (it.hasNext()) {
                     KeyValue<String, StoreItem> entry = it.next();
-                    if (entry.value.isComplete() || entry.value.hasExpired()) {
-//                    kvStore.delete(entry.key);
-                        context.forward(entry.key, entry.value.finalise());
+                    log.trace("entry key {}", entry.key);
+                    if (entry.value.hasExpired()) {
+                        log.trace("XXX Invalidating message key: {} val {} XXX", entry.key, entry.value);
+                        String key = entry.key;
+                        OcrAggregatedEvent event  = entry.value.finalise("expired");
+                        context.forward(key, event );
+                        kvStore.delete(entry.key);
                     }
                 }
                 it.close();
                 // commit the current processing progress
                 context.commit();
             } catch (Exception e) {
-                LOGGER.error("error in punctuator", e.getMessage());
+                log.error("error in punctuator", e.getMessage());
             }
         });
 
     }
 
     @Override
-    public OcrReadyEvent transform(String key, OcrReadyEvent event) {
+    public KeyValue<String, OcrAggregatedEvent> transform(String key, OcrReadyEvent event) {
         try {
-            LOGGER.info("transforming {} msg {} of {}", key, event.getPart(), event.getTotal());
-            if (event.getPart() == event.getTotal()) {
-                return new StoreItem(event).finalise();
+            log.trace("transforming {} msg {} of {}", key, event.getPart(), event.getTotal());
+            StoreItem storeItem = this.kvStore.get(key);
+            if (storeItem == null) {
+                storeItem = new StoreItem(event);
             } else {
-                StoreItem storeItem = this.kvStore.get(key);
-                if (storeItem == null) {
-                    this.kvStore.put(key, new StoreItem(event));
-                } else {
-                    storeItem.addEvent(event);
-                    if (storeItem.isComplete()) {
-                        return storeItem.finalise();
-                    }
-                }
+                storeItem.incrementAndGetCount();
+                storeItem.addEvent(event);
+            }
+            kvStore.put(key, storeItem);
+            if (storeItem.isComplete()) {
+                kvStore.delete(key);
+                return KeyValue.pair(key, storeItem.finalise());
             }
         } catch (Exception e) {
-            LOGGER.error("error in transformer", e.getMessage());
+            log.error("error in transformer");
+            e.printStackTrace();
         }
-            return null;
+        return null;
     }
 
 
