97b0aef7cc352c4a25b437e854539fcdc96ffed7
Armand Ballaci <aballaci@gmail.com>
2020-11-29 12:18:22 +0100

    Update CustomAggregator.java

--- src/main/java/com/ballaci/consumer/CustomAggregator.java
+++ src/main/java/com/ballaci/consumer/CustomAggregator.java
@@ -6,41 +6,29 @@ import com.ballaci.processors.OcrEventTransformer;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Named;
-import org.apache.kafka.streams.kstream.Transformer;
 import org.apache.kafka.streams.kstream.TransformerSupplier;
 import org.apache.kafka.streams.state.StoreBuilder;
 import org.apache.kafka.streams.state.Stores;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
 
 @Component
 public class CustomAggregator {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CustomAggregator.class);
 
-    @Autowired
-    private ApplicationContext applicationContext;
-
     private String stateStoreName = "ocr-store";
 
     @Bean
-    public java.util.function.Consumer<KStream<String, OcrReadyEvent>> customaggregator() {
+    public Function<KStream<String, OcrReadyEvent>, KStream<String, OcrReadyEvent>> customaggregator() {
 
         return input ->
                 input
-                        .peek((k,v) -> LOGGER.info("Peek in transformer: " + k + " Value: " + v))
-                        .transform(new TransformerSupplier(){
-
-                            @Override
-                            public Transformer get() {
-                                return new OcrEventTransformer(stateStoreName);
-                            }
-                        }, Named.as("cor-transformer"), stateStoreName)
-                        .peek((k,v) -> LOGGER.info("Done Aggregation: " + k + " Value: " + v));
+                        .transform((TransformerSupplier) () -> new OcrEventTransformer(stateStoreName), Named.as("ocr-transformer"), stateStoreName);
     }
 
 
