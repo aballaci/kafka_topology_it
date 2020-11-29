d07e961b3b1af8018ab7e63776588a05ad4cf381
Armand Ballaci <aballaci@gmail.com>
2020-11-29 12:54:24 +0100

    SAVEPOINT

--- src/main/java/com/ballaci/processors/OcrEventTransformer.java
+++ src/main/java/com/ballaci/processors/OcrEventTransformer.java
@@ -1,7 +1,6 @@
 package com.ballaci.processors;
 
 
-import com.ballaci.CustomSerdes;
 import com.ballaci.model.OcrAggregatedEvent;
 import com.ballaci.model.OcrReadyEvent;
 import com.ballaci.model.StoreItem;
@@ -12,10 +11,9 @@ import org.apache.kafka.streams.processor.ProcessorContext;
 import org.apache.kafka.streams.processor.PunctuationType;
 import org.apache.kafka.streams.state.KeyValueIterator;
 import org.apache.kafka.streams.state.KeyValueStore;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
+
 @Slf4j
 public class OcrEventTransformer implements Transformer<String, OcrReadyEvent, KeyValue<String, OcrAggregatedEvent>> {
 
@@ -29,38 +27,13 @@ public class OcrEventTransformer implements Transformer<String, OcrReadyEvent, K
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public void init(ProcessorContext context) {
 
-        // keep the processor context locally because we need it in punctuate() and commit()
         this.context = context;
 
-        // retrieve the key-value store named "Counts"
         kvStore = (KeyValueStore) context.getStateStore(stateStoreName);
 
-        // schedule a punctuate() method every second based on event-time
-        this.context.schedule(Duration.ofMillis(1000), PunctuationType.WALL_CLOCK_TIME, (timestamp) -> {
-            try {
-                log.trace("Punctuating....");
-                KeyValueIterator<String, StoreItem> it = this.kvStore.all();
-                while (it.hasNext()) {
-                    KeyValue<String, StoreItem> entry = it.next();
-                    log.trace("entry key {}", entry.key);
-                    if (entry.value.hasExpired()) {
-                        log.trace("XXX Invalidating message key: {} val {} XXX", entry.key, entry.value);
-                        String key = entry.key;
-                        OcrAggregatedEvent event  = entry.value.finalise("expired");
-                        context.forward(key, event );
-                        kvStore.delete(entry.key);
-                    }
-                }
-                it.close();
-                // commit the current processing progress
-                context.commit();
-            } catch (Exception e) {
-                log.error("error in punctuator", e.getMessage());
-            }
-        });
+        this.context.schedule(Duration.ofMillis(1000), PunctuationType.WALL_CLOCK_TIME, (timestamp) -> invalidateExpiredMessages());
 
     }
 
@@ -87,11 +60,30 @@ public class OcrEventTransformer implements Transformer<String, OcrReadyEvent, K
         return null;
     }
 
+    private void invalidateExpiredMessages() {
+        try {
+            log.trace("invalidateExpiredMessages....");
+            KeyValueIterator<String, StoreItem> it = this.kvStore.all();
+            while (it.hasNext()) {
+                KeyValue<String, StoreItem> entry = it.next();
+                log.trace("entry key {}", entry.key);
+                if (entry.value.hasExpired()) {
+                    log.trace("Invalidating message key: {} val {}", entry.key, entry.value);
+                    String key = entry.key;
+                    OcrAggregatedEvent event = entry.value.finalise("expired");
+                    context.forward(key, event);
+                    kvStore.delete(entry.key);
+                }
+            }
+            it.close();
+            context.commit();
+        } catch (Exception e) {
+            log.error("error in punctuator", e.getMessage());
+        }
+    }
+
 
     @Override
     public void close() {
-        // close any resources managed by this processor.
-        // Note: Do not close any StateStores as these are managed
-        // by the library
     }
 };
